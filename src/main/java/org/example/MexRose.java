e71ac2e8734dd666882bb4f2947fcbcb3c86fa3c
ankitmishra <meankitmishra10@gmail.com>
2025-10-16 00:52:03 +0530

    16-Oct-2025 session 1

---
+++ src/main/java/org/example/MexRose.java
@@ -0,0 +1,36 @@
+//package org.example;
+
+import java.util.Scanner;
+
+public class MexRose {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int[] arr = new int[n];
+            int[] count = new int[n+1];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+                count[arr[i]]++;
+            }
+            int mex = Integer.MAX_VALUE;
+            int ans = 0;
+            for (int i = 0; i < k; i++) {
+                if (count[i] == 0) {
+                    ans++;
+                    if (count[k] > 0) {
+                        count[k]--;
+                    }
+                }
+            }
+            while (count[k] > 0) {
+                ans++;
+                count[k]--;
+            }
+            System.out.println(ans);
+        }
+
+    }
+}
